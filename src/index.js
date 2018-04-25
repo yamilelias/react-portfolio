c8cedf8c03924a5911279287cad98b25df7a3ec3
Yamil Elías <yamileliassoto@gmail.com>
2018-04-24 22:34:11 -0600

    :building_construction: :bento: added template and missing styles

--- src/index.js
+++ src/index.js
@@ -1,6 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+
+import 'bootstrap/dist/css/bootstrap.min.css';
 import './style/index.scss';
+
 import App from './components/App';
 import registerServiceWorker from './registerServiceWorker';
 
