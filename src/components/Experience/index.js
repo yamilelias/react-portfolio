660b774de0d330e1bc5fd1a84c121a2e0df3d043
Yamil Elías <yamileliassoto@gmail.com>
2018-05-04 19:40:45 -0600

    :bug: fixed props error in some views

--- src/components/Experience/index.js
+++ src/components/Experience/index.js
@@ -5,48 +5,50 @@ import ResumeContent from './ResumeContent';
 const Experience = () => {
   return (
     <Section id="experience">
-      <Section.Header text="Experience"/>
-      <ResumeContent
-        title="Senior Web Developer"
-        company="Intelitec Solutions"
-        date="March 2013 - Present">
-        Bring to the table win-win survival strategies to ensure
-        proactive domination. At the end of the day, going forward,
-        a new normal that has evolved from generation X is on the
-        runway heading towards a streamlined cloud solution. User
-        generated content in real-time will have multiple
-        touchpoints for offshoring.
-      </ResumeContent>
-      <ResumeContent
-        title="Web Developer"
-        company="Intelitec Solutions"
-        date="December 2011 - March 2013">
-        Capitalize on low hanging fruit to identify a ballpark
-        value added activity to beta test. Override the digital
-        divide with additional clickthroughs from DevOps.
-        Nanotechnology immersion along the information highway will
-        close the loop on focusing solely on the bottom line.
-      </ResumeContent>
-      <ResumeContent
-        title="Junior Web Designer"
-        company="Shout! Media Productions"
-        date="July 2010 - December 2011">
-        Podcasting operational change management inside of
-        workflows to establish a framework. Taking seamless key
-        performance indicators offline to maximise the long tail.
-        Keeping your eye on the ball while performing a deep dive on
-        the start-up mentality to derive convergence on
-        cross-platform integration.
-      </ResumeContent>
-      <ResumeContent
-        title="Web Design Intern"
-        company="Shout! Media Productions"
-        date="September 2008 - June 2010">
-        Collaboratively administrate empowered markets via
-        plug-and-play networks. Dynamically procrastinate B2C users
-        after installed base benefits. Dramatically visualize
-        customer directed convergence without revolutionary ROI.
-      </ResumeContent>
+      <div>
+        <Section.Header text="Experience"/>
+        <ResumeContent
+          title="Senior Web Developer"
+          company="Intelitec Solutions"
+          date="March 2013 - Present">
+          Bring to the table win-win survival strategies to ensure
+          proactive domination. At the end of the day, going forward,
+          a new normal that has evolved from generation X is on the
+          runway heading towards a streamlined cloud solution. User
+          generated content in real-time will have multiple
+          touchpoints for offshoring.
+        </ResumeContent>
+        <ResumeContent
+          title="Web Developer"
+          company="Intelitec Solutions"
+          date="December 2011 - March 2013">
+          Capitalize on low hanging fruit to identify a ballpark
+          value added activity to beta test. Override the digital
+          divide with additional clickthroughs from DevOps.
+          Nanotechnology immersion along the information highway will
+          close the loop on focusing solely on the bottom line.
+        </ResumeContent>
+        <ResumeContent
+          title="Junior Web Designer"
+          company="Shout! Media Productions"
+          date="July 2010 - December 2011">
+          Podcasting operational change management inside of
+          workflows to establish a framework. Taking seamless key
+          performance indicators offline to maximise the long tail.
+          Keeping your eye on the ball while performing a deep dive on
+          the start-up mentality to derive convergence on
+          cross-platform integration.
+        </ResumeContent>
+        <ResumeContent
+          title="Web Design Intern"
+          company="Shout! Media Productions"
+          date="September 2008 - June 2010">
+          Collaboratively administrate empowered markets via
+          plug-and-play networks. Dynamically procrastinate B2C users
+          after installed base benefits. Dramatically visualize
+          customer directed convergence without revolutionary ROI.
+        </ResumeContent>
+      </div>
     </Section>
   );
 };
--- src/components/Skills/index.js
+++ src/components/Skills/index.js
@@ -5,41 +5,43 @@ import DevList from './DevList';
 const Skills = () => {
   return (
     <Section id="skills">
-      <Section.Header text="Skills"/>
-      <div className="subheading mb-3">Programming Languages &amp; Tools</div>
-      <ul className="list-inline list-icons">
-        <DevList icon="html5"/>
-        <DevList icon="css3"/>
-        <DevList icon="js"/>
-        <DevList icon="jquery"/>
-        <DevList icon="sass"/>
-        <DevList icon="less"/>
-        <DevList icon="bootstrap"/>
-        <DevList icon="wordpress"/>
-        <DevList icon="grunt"/>
-        <DevList icon="gulp"/>
-        <DevList icon="npm"/>
-      </ul>
+      <div>
+        <Section.Header text="Skills"/>
+        <div className="subheading mb-3">Programming Languages &amp; Tools</div>
+        <ul className="list-inline list-icons">
+          <DevList icon="html5"/>
+          <DevList icon="css3"/>
+          <DevList icon="js"/>
+          <DevList icon="jquery"/>
+          <DevList icon="sass"/>
+          <DevList icon="less"/>
+          <DevList icon="bootstrap"/>
+          <DevList icon="wordpress"/>
+          <DevList icon="grunt"/>
+          <DevList icon="gulp"/>
+          <DevList icon="npm"/>
+        </ul>
 
-      <div className="subheading mb-3">Workflow</div>
-      <ul className="fa-ul mb-0">
-        <li>
-          <i className="fa-li fa fa-check"></i>
-          Mobile-First, Responsive Design
-        </li>
-        <li>
-          <i className="fa-li fa fa-check"></i>
-          Cross Browser Testing &amp; Debugging
-        </li>
-        <li>
-          <i className="fa-li fa fa-check"></i>
-          Cross Functional Teams
-        </li>
-        <li>
-          <i className="fa-li fa fa-check"></i>
-          Agile Development &amp; Scrum
-        </li>
-      </ul>
+        <div className="subheading mb-3">Workflow</div>
+        <ul className="fa-ul mb-0">
+          <li>
+            <i className="fa-li fa fa-check"></i>
+            Mobile-First, Responsive Design
+          </li>
+          <li>
+            <i className="fa-li fa fa-check"></i>
+            Cross Browser Testing &amp; Debugging
+          </li>
+          <li>
+            <i className="fa-li fa fa-check"></i>
+            Cross Functional Teams
+          </li>
+          <li>
+            <i className="fa-li fa fa-check"></i>
+            Agile Development &amp; Scrum
+          </li>
+        </ul>
+      </div>
     </Section>
   );
 };
