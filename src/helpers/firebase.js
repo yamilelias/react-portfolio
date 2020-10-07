fed8b7b1e5a1faeb9c06808c08b57cb8f56344bc
Yamil Elías <yamil.elias@delpunto.com>
2020-10-06 20:49:41 -0600

    Solved dependency issues

--- src/helpers/firebase.js
+++ src/helpers/firebase.js
@@ -1,12 +1,20 @@
-import firebase from 'firebase';
+import * as firebase from "firebase/app";
+
+// If you enabled Analytics in your project, add the Firebase SDK for Analytics
+import "firebase/analytics";
+
+// Add the Firebase products that you want to use
+import "firebase/auth";
+import "firebase/database";
 
 export const config = {
-  apiKey: 'AIzaSyCeOd7dnJGLl0Slt0kZYzUNpD1bulyOJQo',
-  authDomain: 'yamilelias-portfolio.firebaseapp.com',
-  databaseURL: 'https://yamilelias-portfolio.firebaseio.com',
-  projectId: 'yamilelias-portfolio',
-  storageBucket: 'yamilelias-portfolio.appspot.com',
-  messagingSenderId: '984767434388',
+  apiKey: "AIzaSyCuN3Vml--DFFvmCH4E3d-3y5a2T7_w-sY",
+  authDomain: "yamilelias-portfolio.firebaseapp.com",
+  databaseURL: "https://yamilelias-portfolio.firebaseio.com",
+  projectId: "yamilelias-portfolio",
+  storageBucket: "yamilelias-portfolio.appspot.com",
+  messagingSenderId: "984767434388",
+  appId: "1:984767434388:web:dc9298c0d7c6da7296ee81"
 };
 
 export const fireapp = firebase.initializeApp(config);
--- src/index.js
+++ src/index.js
@@ -9,8 +9,9 @@ import rootReducer from './reducers';
 
 // Uncomment for Redux dev tools (https://github.com/zalmoxisus/redux-devtools-extension)
 /* eslint-disable */
-const composeEnhancers = typeof window === 'object' && window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ ? window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__({ }) : compose;
-const enhancer = composeEnhancers(applyMiddleware(thunkMiddleware));
+// const composeEnhancers = typeof window === 'object' && window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ ? window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__({ }) : compose;
+// const enhancer = composeEnhancers(applyMiddleware(thunkMiddleware));
+const enhancer = applyMiddleware(thunkMiddleware);
 const store = createStore(rootReducer, enhancer);
 /* eslint-enable */
 
